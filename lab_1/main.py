acadfb8dd4da987691b2b132f3ea60882d9c8847
ValeriaPopinova <valerie.popinova19@gmail.com>
2019-10-06 20:42:43 +0300

    ready version

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,19 +5,40 @@ Count frequencies dictionary by the given arbitrary text
 
 
 def calculate_frequences(text: str) -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    return {'word': 10}
+    freq_dict = {}
+    if text == '' or text is None:
+        return freq_dict
+    text = str(text)
+    text1 = text.lower()
+    for i in text1:
+        if not i.isalpha():
+            text1 = text1.replace(i, ' ')
+    text_new = text1.split()
+    for word in text_new:
+        if word.isalpha() and word not in freq_dict.keys():
+            dict_key = word
+            dict_value = text_new.count(word)
+            new_dict = {dict_key: dict_value}
+            freq_dict.update(new_dict)
+    return freq_dict
+    print(calculate_frequences(text))
 
-def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    pass
 
-def get_top_n(frequencies: dict, top_n: int) -> tuple:
-    """
-    Takes first N popular words
-    """
-    pass
+def filter_stop_words(freq_dict, stop_words: tuple) -> dict:
+    filter_dict = {}
+    if freq_dict is not None and stop_words is not None:
+        for key, value in freq_dict.items():
+            if key == str(key):
+                if key not in stop_words:
+                    filter_dict.update({key: value})
+    return filter_dict
+    print(filter_stop_words(freq_dict, stop_words))
+
+
+def get_top_n(filter_dict, number: int) -> tuple:
+    if number <= 0:
+      return()
+    new_list = sorted(filter_dict, key=filter_dict.get, reverse=True)
+    print(new_list)
+    return tuple(new_list[:number])
+    print(get_top_n(filter_dict, number))
